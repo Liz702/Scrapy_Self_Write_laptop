2639fac51cb759d7c1cafd40bba906d0fd5aa244
Liz702 <73422987+Liz702@users.noreply.github.com>
2025-05-29 20:05:50 +0800

    Add files via upload

--- amazon-python-scrapy-scraper-master-20250206T032408Z-001/amazon-python-scrapy-scraper-master/amazon/spiders/amazon_search_product_selfwrite.py
+++ amazon-python-scrapy-scraper-master-20250206T032408Z-001/amazon-python-scrapy-scraper-master/amazon/spiders/amazon_search_product_selfwrite.py
@@ -2,6 +2,7 @@ import json
 import scrapy
 from urllib.parse import urljoin
 import re
+import logging
 
 class AmazonSearchProductSpider(scrapy.Spider):
     name = "amazon_search_product"
@@ -12,6 +13,10 @@ class AmazonSearchProductSpider(scrapy.Spider):
         keyword_list = ['ipad']
         for keyword in keyword_list:
             amazon_search_url = f'https://www.amazon.com/s?k={keyword}&page=1'
+
+            self.logger.info("开始爬取网页")
+            self.logger.in
+
             yield scrapy.Request(url=amazon_search_url, callback=self.discover_product_urls, meta={'keyword': keyword, 'page': 1})
 
     def discover_product_urls(self, response):
